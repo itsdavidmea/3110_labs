5182829ee1ac5dd0f9327566a196c7f609e4eea8
David Mea <mea.david29@outlook.com>
2025-09-15 15:38:36 -0400

    Removed buddy from address book

--- Lab1/main.java
+++ Lab1/main.java
@@ -4,7 +4,7 @@ public static void main(String[] args) {
 
     BuddyInfo aBuddy = new BuddyInfo("David");
     AddressBook aBook = new AddressBook();
-    aBook.addBuddy(aBuddy);
+
 
     System.out.println("Address Book");
 }
